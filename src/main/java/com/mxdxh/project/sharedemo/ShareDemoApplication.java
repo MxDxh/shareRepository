06d2d0f1d1435a90816f3e04069b77932d51d40b
pyh <1250571741@qq.com>
2026-07-01 18:24:31 +0800

    测试提交

---
+++ src/main/java/com/mxdxh/project/sharedemo/ShareDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mxdxh.project.sharedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mxdxh/project/sharedemo/ShareDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxdxh.project.sharedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
